44a67a3aa4255dc2d3b2df27882543081fb3ee03
LILYSLU <152039845+LILYSLU@users.noreply.github.com>
2024-11-07 21:27:54 +0000

    main

---
+++ main.java
@@ -0,0 +1,23 @@
+
+
+import java.util.Scanner; // Allows us to use Java's Scanner class
+import java.io.File;
+
+public class Main {
+  // Declare a story variable that all methods in the class can uses
+  public static String story;
+
+  public static void main(String[] args) {
+    // Scanners allow code to read user input
+    File madLib = new File("November Madlibs.txt");
+    Scanner myReader = new Scanner(madLib);
+    // Create variables for words the user will enter
+    String season;
+    String verb;
+    String adjective;
+    String noun;
+    String timeSpan;
+
+    
+}
+}
